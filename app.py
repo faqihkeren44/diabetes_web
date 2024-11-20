2262872f4687379f06dc36f11c79d556389c7873
faqihkeren44 <149081499+faqihkeren44@users.noreply.github.com>
2024-11-20 10:23:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
-from flask import render_template, request
+from flask import Flask, render_template, request
 import pickle
 import pandas as pd
 
 with open("model.pkl", "rb") as model_file:
     model = pickle.load(model_file)
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 
 features = ['blood_glucose_level', 'HbA1c_level', 'age', 'bmi']
 
@@ -16,14 +15,21 @@ def main():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data1 = request.form['a']
-    data2 = request.form['b']
-    data3 = request.form['c']
-    data4 = request.form['d']
-    arr = [[data1, data2, data3, data4]]
+    blood_glucose_level = request.form['a']
+    HbA1c_level = request.form['b']
+    age = request.form['c']
+    bmi = request.form['d']
+    name = request.form['e']
+    arr = [[blood_glucose_level, HbA1c_level, age, bmi]]
     arr = pd.DataFrame(arr, columns=features)
     pred = model.predict(arr)
-    return render_template('after.html', data=pred)
+    return render_template('after.html',
+                           result=pred,
+                           name=name,
+                           blood_glucose_level=blood_glucose_level,
+                           HbA1c_level=HbA1c_level,
+                           age=age,
+                           bmi=bmi)
     
 if __name__ == "__main__":
     app.run(debug=True)
